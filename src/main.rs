79e97d19a328fef50587e1733217d6dbfd8a6a03
Lloyd Lobo <76430758+lloydlobo@users.noreply.github.com>
2023-01-08 12:15:00 +0530

    feat: wip setup ui module

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,15 @@ mod ui;
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
-    window::PresentMode,
+    window::{CursorGrabMode, PresentMode},
 };
 use input::*;
 use simulation::*;
 use ui::*;
 
 const GRID_SIZE: i32 = 100;
+
+//-------------------------------------------------------------------------------------------------
 
 /// NOTE: Since bevy 0.9 - The `WindowDescriptor` settings have been moved
 /// from a resource to `WindowPlugin::window`.
@@ -37,9 +39,15 @@ fn main() {
         // .add_plugins(SimulationPlugin)
         // .add_plugins(InputPlugin)
         // .add_plugins(MainMenuPlugin)
+        //bevy window stock systems.
+        .add_system(change_title)
+        .add_system(toggle_cursor)
         .add_system(toggle_vsync)
+        .add_system(cycle_cursor_icon)
         .run();
 }
+
+//-------------------------------------------------------------------------------------------------
 
 /// This system toggles the vsync mode when pressing the button V.
 /// You'll see fps increase displayed in the console.
@@ -56,3 +64,53 @@ fn toggle_vsync(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
         info!("PRESENT_MODE: {:?}", window.present_mode());
     }
 }
+
+/// This system will then change the title during execution
+fn change_title(time: Res<Time>, mut windows: ResMut<Windows>) {
+    let window = windows.primary_mut();
+    window.set_title(format!(
+        "Game of Life: Seconds since startup: {}",
+        time.elapsed_seconds().round()
+    ));
+}
+
+/// This system toggles the cursor's visibility when the space bar is pressed
+fn toggle_cursor(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
+    let window = windows.primary_mut();
+    if input.just_pressed(KeyCode::Space) {
+        window.set_cursor_grab_mode(match window.cursor_grab_mode() {
+            CursorGrabMode::None => CursorGrabMode::Locked,
+            CursorGrabMode::Locked | CursorGrabMode::Confined => CursorGrabMode::None,
+        });
+        window.set_cursor_visibility(!window.cursor_visible());
+    }
+}
+
+/// This system cycles the cursor's icon through a small set of icons when clicking
+fn cycle_cursor_icon(
+    input: Res<Input<MouseButton>>,
+    mut windows: ResMut<Windows>,
+    mut index: Local<usize>,
+) {
+    const ICONS: &[CursorIcon] = &[
+        CursorIcon::Default,
+        CursorIcon::Hand,
+        CursorIcon::Wait,
+        CursorIcon::Text,
+        CursorIcon::Copy,
+    ];
+    let window = windows.primary_mut();
+    if input.just_pressed(MouseButton::Left) {
+        *index = (*index + 1) % ICONS.len();
+        window.set_cursor_icon(ICONS[*index]);
+    } else if input.just_pressed(MouseButton::Right) {
+        *index = if *index == 0 {
+            ICONS.len() - 1
+        } else {
+            *index - 1
+        };
+        window.set_cursor_icon(ICONS[*index]);
+    }
+}
+
+//-------------------------------------------------------------------------------------------------
--- src/ui.rs
+++ src/ui.rs
@@ -1,1 +1,117 @@
 //! This module implements the UI for the game of life.
+//!
+//! [Reference](https://github.com/bevyengine/bevy/blob/latest/examples/ui/button.rs)
+//!
+//! NOTE: Inverted y axis. everything goes from bottom-up, not top-down.
+
+use bevy::prelude::*;
+
+const NORMAL_BUTTON: Color = Color::rgb(0.8, 0.8, 0.8);
+const HOVERED_BUTTON: Color = Color::rgb(0.4, 0.8, 0.8);
+const PRESSED_BUTTON: Color = Color::rgb(0.4, 1.0, 1.0);
+
+impl Plugin for MainMenuPlugin {
+    /// Build function similar to that in main setup.
+    fn build(&self, app: &mut App) {
+        app.add_event::<GameExitEvent>()
+            .add_event::<SimulationStartEvent>()
+            .add_event::<SimulationStopEvent>()
+            .add_startup_system(startup_system_setup)
+            .add_system(button_system);
+    }
+}
+
+pub struct GameExitEvent;
+
+pub struct SimulationStartEvent;
+
+pub struct SimulationStopEvent;
+
+#[derive(Component)]
+struct ClassicButton(ButtonType);
+
+#[derive(PartialEq, Copy, Clone)]
+enum ButtonType {
+    Start,
+
+    Stop,
+
+    Exit,
+}
+
+pub struct MainMenuPlugin;
+
+//-------------------------------------------------------------------------------------------------
+
+/// A UI node that is a button
+fn build_classic_button(asset_server: &Res<AssetServer>) -> ButtonBundle {
+    ButtonBundle {
+        style: Style { ..default() },
+        // ? deprecated: color.
+        background_color: NORMAL_BUTTON.into(),
+        image: UiImage(asset_server.load("sprites/button.png")),
+        ..default()
+    }
+}
+
+fn build_classic_text(value: &str, asset_server: &Res<AssetServer>) -> TextBundle {
+    TextBundle {
+        // ? deprecated: `Text::with_section`
+        text: Text::from_section(
+            value,
+            TextStyle {
+                font: asset_server.load("fonts/Symtext.ttf"),
+                font_size: 30.0,
+                color: Color::rgb(0.9, 0.9, 0.9),
+            },
+        ),
+        ..default()
+    }
+}
+
+//-------------------------------------------------------------------------------------------------
+
+// pub struct Query<'world, 'state, Q: WorldQuery, F: ReadOnlyWorldQuery = ()>
+#[allow(clippy::type_complexity)]
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, &ClassicButton),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut start_writer: EventWriter<SimulationStartEvent>,
+    mut stop_writer: EventWriter<SimulationStopEvent>,
+    mut exit_writer: EventWriter<GameExitEvent>,
+) {
+    for (interaction, mut color, classic_button) in interaction_query.iter_mut() {
+        match *interaction {
+            Interaction::Clicked => {
+                *color = PRESSED_BUTTON.into();
+                match classic_button.0 {
+                    ButtonType::Start => {
+                        start_writer.send(SimulationStartEvent);
+                    }
+                    ButtonType::Stop => {
+                        stop_writer.send(SimulationStopEvent);
+                    }
+                    ButtonType::Exit => {
+                        exit_writer.send(GameExitEvent);
+                    }
+                }
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+//-------------------------------------------------------------------------------------------------
+
+/// AssetServer is the backend that hosts the events like Start, Stop..
+fn startup_system_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // UI camera.
+    commands.spawn(Camera2dBundle::default());
+}
